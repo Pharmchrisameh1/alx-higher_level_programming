e6f8b5325eb7266b0dd08ea9aec4c2ea30d55e9e
Rx Chris Ameh <chris1jnr@gmail.com>
2024-09-12 11:40:41 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    """Print the number of and list of arguments."""
     import sys
-    len = len(sys.argv)
 
-    if len != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    operators = ["+", "-", "*", "/"]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
-
-    if operator not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
