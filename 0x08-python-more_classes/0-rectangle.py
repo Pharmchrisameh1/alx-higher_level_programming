c9d6a0c10ca23b20b69e27f0149a42bd1b065eeb
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-07 10:26:33 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Task 0 of ALX Project(Python - More Classes and Objects)
+
+This module creates a class ``Rectangle``
+
+"""
+
+
+class Rectangle:
+    """Do nothing"""
+    pass
