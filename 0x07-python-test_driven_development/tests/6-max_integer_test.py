ed01cca93da0ae9a4dc116a1e8ded933e1d7c520
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-09 05:42:25 -0700

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,75 +1,53 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
-
+"""Unittest for max_integer([..])."""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Class that tests the max_integer module
-    """
-    def test_emptylist(self):
-        """Tests the case if the list is empty.
-        """
-        maxint = max_integer([])
-        self.assertEqual(maxint, None)
+    """A test suite for the max_integer function."""
 
-    def test_stringsinlist(self):
-        """Tests the case if the list has strings.
-        """
-        maxint = max_integer(["Hi", "Hey", "Hello"])
-        self.assertEqual(maxint, "Hi")
+    def test_empty_list(self) -> None:
+        """Test when given an empty list."""
+        self.assertEqual(max_integer([]), None)
 
-    def test_onestringasinput(self):
-        """Tests the case if only a string is given
-        to function.
-        """
-        maxint = max_integer("HELLO")
-        self.assertEqual(maxint, 'O')
+    def test_no_arg(self) -> None:
+        """Test when no argument is provided."""
+        self.assertEqual(max_integer(), None)
 
-    def test_floatnumberinlist(self):
-        """Tests if the list has a floatnumber
-        """
-        maxint = max_integer([1, -4, 5, 15.8])
-        self.assertEqual(maxint, 15.8)
+    def test_sorted_list(self) -> None:
+        """Test when given a sorted list of integers."""
+        self.assertEqual(max_integer([1, 3, 4, 7]), 7)
 
-    def test_allfloatlist(self):
-        """Tests an all float list
-        """
-        maxint = max_integer([1.4, -5.5, 23.6])
-        self.assertEqual(maxint, 23.6)
+    def test_unsorted_list(self) -> None:
+        """Test when given an unsorted list of integers."""
+        self.assertEqual(max_integer([2, 8, 9, 2, 1]), 9)
 
-    def test_stringinintlist(self):
-        """Tests if the list has a string among ints & floats
-        """
-        self.assertRaises(TypeError, max_integer, [1, 5.4, "HI", [3, 2, 5]])
+    def test_max_first_lis(self) -> None:
+        """Test when Max number is at beginning."""
+        self.assertEqual(max_integer([8, 5, 1]), 8)
 
-    def test_singleelementlist(self):
-        """Tests if the list has a single element.
-        """
-        maxint = max_integer([30])
-        self.assertEqual(maxint, 30)
+    def test_only_one(self) -> None:
+        """Test when there's only one element in the list."""
+        self.assertEqual(max_integer([8]), 8)
 
-    def test_tupleasinput(self):
-        """Tests a tuple as input
-        """
-        maxint = max_integer((5, 4, -4, 45.7))
-        self.assertEqual(maxint, 45.7)
+    def test_float_list(self) -> None:
+        """Text when given a list of float number."""
+        self.assertEqual(max_integer([1.5, 4.5, 0.2]), 4.5)
 
-    def test_dictasinput(self):
-        """Tests a dict as input
-        """
-        a = {'a': 1, 2: 4.3, 3: 'Hi', 'Hey': 3}
-        self.assertRaises(KeyError, max_integer, a)
+    def test_mix_nbr_list(self) -> None:
+        """Test when given a list of mixed numeric types."""
+        self.assertEqual(max_integer([1.05, 4.5, 8, 7.9, 7, 4]), 8)
 
-    def test_emptystring(self):
-        """Tests an empty string as input
-        """
-        maxint = max_integer("")
-        self.assertEqual(maxint, None)
+    def test_nigative_int_list(self) -> None:
+        """Text when given a list with nigative numbers."""
+        self.assertEqual(max_integer([-4, -5, -1, -9]), -1)
 
+    def test_empty_string(self) -> None:
+        """Test when an empty string is provided."""
+        self.assertEqual(max_integer(""), None)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_str_list(self) -> None:
+        """Test when given a list of strings."""
+        self.assertEqual(max_integer(["a9bc", "abc7", "1abc"]), "abc7")
