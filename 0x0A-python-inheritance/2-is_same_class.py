27a4128d9f3ae495ba1ecb025077b7f20fa511f9
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-14 13:28:21 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-"""Checks if object is an instance of a class"""
+"""Task 2 of ALX Project(Python - Inheritance)
+
+This module defines a function that confirms an object-class relationship.
+
+"""
 
 
 def is_same_class(obj, a_class):
-    """Return true if object is an instance of the
-    class, otherwise return false
+    """Check if the specified object is exactly an instance of
+    the specified class.
+
+    Args:
+        obj: the object
+        a_class: the class
+
+    Returns:
+        bool: True, if there's a direct relationship, and False, if otherwise.
+
     """
-    return (type(obj) == a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
