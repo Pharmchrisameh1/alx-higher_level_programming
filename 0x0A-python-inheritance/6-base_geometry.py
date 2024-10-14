bb83c36b1d20328800d405c7850fe0cead40d60a
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-14 14:11:30 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 6 of ALX Project(Python - Inheritance)
+
+This module defines a class which has an instance method
+
+"""
+
+
+class BaseGeometry:
+    """Do nothing meaningful"""
+
+    def area(self):
+        """Raise an error"""
+        raise Exception("area() is not implemented")
