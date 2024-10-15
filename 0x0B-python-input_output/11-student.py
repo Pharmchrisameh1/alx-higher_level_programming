ff4bb57a376f844c3542a7dc24c8f5767bc57b7f
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-15 05:57:45 -0700

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
-"""Task 11 of ALX Project(Python - Input/Output)
-
-This module defines a student class.
-
+"""This module defines the class ``Student``.
 """
 
 
 class Student:
-    """Represent a student"""
-
+    """This class represents a Student object.
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student"""
+        """This method initiates the Student class instance
+        with the values (first_name, last_name, age)
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The student's age in years.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of a Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
+        """This method retrieves a dictionary representation of a
+        student instance.
         Args:
-            attrs (list): (Optional) The attributes to represent.
-
+            attrs (list): optional argument list of strings.
         """
-        if (type(attrs) == list and
-                all(type(attr) == str for attr in attrs)):
-            return {attr: self.__dict__.get(attr) for attr in attrs
-                    if attr in self.__dict__}
-        return self.__dict__
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            picked_dict = dict()
+            for key in attrs:
+                if key in self.__dict__.keys():
+                    picked_dict[key] = self.__dict__.get(key)
+            return picked_dict
 
     def reload_from_json(self, json):
-        """Replace all attributes of a Student.
-
+        """This method replaces all attributes of student instance.
         Args:
-            json (dict): The attributes:values to replace attributes with
-
+            json (str): The json representation of the dict that'll
+                replace the Student instance attributes.
         """
         for key, value in json.items():
-            setattr(self, key, value)
+            self.__dict__[key] = value
