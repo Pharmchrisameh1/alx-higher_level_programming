44275af1bcdad1cd45abf7b4a3df9ae1df0396cc
Rx Chris Ameh <chris1jnr@gmail.com>
2024-12-20 14:52:43 -0800

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square.js');
+module.exports = class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log();
+      }
+    }
+  }
+};
