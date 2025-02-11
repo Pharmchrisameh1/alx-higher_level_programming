0cfb367a9fd70a0e07d3ac31faf8c4a8f6992c81
Rx Chris Ameh <chris1jnr@gmail.com>
2025-02-11 08:49:30 -0800

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
-// this script gets the contents of a webpage and stores it in a file.
+// this script computes the number of tasks completed by user id.
 const request = require('request');
-const fs = require('fs');
 const url = process.argv[2];
-const filename = process.argv[3];
 
-if (process.argv.length === 4) {
+if (process.argv.length === 3) {
   request(url, (err, response, body) => {
     if (err) {
       console.error(err);
-      return;
     }
-    fs.writeFile(filename, body, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
+    const result = JSON.parse(body);
+    const total = {};
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].completed === true) {
+        if (result[i].userId in total) {
+          total[result[i].userId] = (total[result[i].userId] + 1);
+        } else {
+          total[result[i].userId] = 1;
+        }
       }
-    });
+    }
+    console.log(total);
   });
 }
