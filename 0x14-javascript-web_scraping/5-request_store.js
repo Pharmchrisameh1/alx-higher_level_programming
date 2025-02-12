9f1ec4871241a3bf2afc346c3e0ae8297d3e5cb1
Rx Chris Ameh <chris1jnr@gmail.com>
2025-02-12 04:58:38 -0800

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
-// this script computes the number of tasks completed by user id.
-const request = require('request');
-const url = process.argv[2];
 
-if (process.argv.length === 3) {
-  request(url, (err, response, body) => {
-    if (err) {
-      console.error(err);
-    }
-    const result = JSON.parse(body);
-    const total = {};
-    for (let i = 0; i < result.length; i++) {
-      if (result[i].completed === true) {
-        if (result[i].userId in total) {
-          total[result[i].userId] = (total[result[i].userId] + 1);
-        } else {
-          total[result[i].userId] = 1;
-        }
-      }
-    }
-    console.log(total);
-  });
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: ./5-request_store.js <URL> <filePath>');
+  process.exit(1);
 }
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error fetching ${url}:`, error);
+    process.exit(1);
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error writing to file ${filePath}:`, err);
+      process.exit(1);
+    }
+  });
+});
+
